69d1394bed2773412be0f72ab32dc62e0599844e
ElaheSmailpour <eli_es87@yahoo.com>
2022-04-17 14:20:52 +0200

    todoList

---
+++ index.js
@@ -0,0 +1,19 @@
+
+
+function addToDo(){
+document.getElementById("todolist").innerHTML+=`
+<li class="mdl-list__item">
+<span class="mdl-list__item-primary-content">
+  <i class="material-icons  mdl-list__item-avatar">label</i>
+ ${todofiled.value}
+</span>
+<span class="mdl-list__item-secondary-action">
+  <label class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect" for="list-checkbox-1">
+    <input type="checkbox" id="list-checkbox-1" class="mdl-checkbox__input" checked />
+  </label>
+</span>
+</li>
+`;
+todofiled.value="";
+}
+             
